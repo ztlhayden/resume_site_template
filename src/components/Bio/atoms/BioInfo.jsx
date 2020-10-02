5a712fe1217c13daee0a1c11220e6bd572403cf9
Hayden Young <haydenyounglex@ohtmail.com>
2020-10-02 15:18:32 -0400

    fix broken queries from adding resolute json

--- src/components/Bio/atoms/BioInfo.jsx
+++ src/components/Bio/atoms/BioInfo.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import { useStaticQuery, graphql } from "gatsby"
 
 const BioInfo = () => {
-  const data = useStaticQuery(graphql`
-  {
-    site {
-      resumeData {
+  const json = useStaticQuery(graphql`
+    {
+      resumeDataJson {
         name
         title
         bio
       }
     }
-  }
-`)
+  `)
+  const data = json.resumeDataJson
+
   return (
     <div className="bio_info">
-      <h1 className="bio_name">{data.site.resumeData.name}</h1>
-      <span className="bio_title">{data.site.resumeData.title}</span>
-      <p className="bio_objective">{data.site.resumeData.bio}</p>
+      <h1 className="bio_name">{data.name}</h1>
+      <span className="bio_title">{data.title}</span>
+      <p className="bio_objective">{data.bio}</p>
     </div>
   )
 }
