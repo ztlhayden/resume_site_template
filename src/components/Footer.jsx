53bbdaa66c241a82f32aef019748991afb2c01ea
Hayden Young <haydenyounglex@ohtmail.com>
2020-10-04 17:49:33 -0400

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Contact from './Contact/Contact'
+
+const Footer = () => {
+  return (
+    <div className="footer_wrapper">
+      <Contact />
+      <button>back to top</button>
+    </div>
+  )
+}
+
+export default Footer
