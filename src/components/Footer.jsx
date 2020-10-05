33aeb0da17aba132eeb613b48ccab5d6ca5aa3aa
Hayden Young <haydenyounglex@ohtmail.com>
2020-10-04 20:53:20 -0400

    added scrolling button clickhandling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 import Contact from './Contact/Contact'
 
 const Footer = () => {
+
   return (
     <div className="footer_wrapper">
       <Contact />
-      <button>Back</button>
+      <button onClick={() => window.scrollTo({top: 0, behavior: "smooth"})}>Back</button>
     </div>
   )
 }
